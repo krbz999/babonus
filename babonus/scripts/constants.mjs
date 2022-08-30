5d8d9f47ec5b3adae25312415b4d555c92a539e4
Zhell <50169243+krbz999@users.noreply.github.com>
2022-08-30 19:16:24 +0200

    Delete babonus directory

--- babonus/scripts/constants.mjs
+++
@@ -1,3 +0,0 @@
-export const MATCH = { ANY: "ANY", ALL: "ALL" }
-export const handlingRegular = ["baseWeapons", "damageTypes", "spellSchools", "abilities", "attackTypes", "spellLevels", "saveAbilities"];
-export const handlingSpecial = ["spellComponents", "weaponProperties-needed", "weaponProperties-unfit", "arbitraryComparison"];
